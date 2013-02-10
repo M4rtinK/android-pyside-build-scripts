6c6c4cb23176dc241889b2925c82dabebfb4eb09
Martin Kolman <martin.kolman@gmail.com>
2013-02-10 20:53:25 +0100

    fix checkout & folder creation

--- prepare.sh
+++ prepare.sh
@@ -3,15 +3,19 @@ echo "= preparing folder for PySide for Android build ="
 echo "cloning Shiboken from Git"
 git clone git@github.com:M4rtinK/shiboken-android.git
 echo "switching to known-working commit"
+cd shiboken-android
 git checkout 72aeaf7d39a50ae31dd77a011e8913dce4606480
+cd ..
 
 echo "cloning PySide from Git"
 git clone https://github.com/M4rtinK/pyside-android
 echo "switching to known-working commit"
+cd pyside-android
 git checkout e0cfe7e768df33600d0e5114b84040650be5f582
+cd ..
 
 echo "creating needed folders"
 mkdir "stage"
-mkdir "shiboken build"
+mkdir "shiboken-build"
 mkdir "pyside-build"
 echo "folder prepared for build, continue with build.sh"
