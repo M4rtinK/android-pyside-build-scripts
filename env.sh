69cdaa13392171458e9f1665bb992ba0d3037657
Martin Kolman <martin.kolman@gmail.com>
2013-02-21 23:16:00 +0100

    fix for ld not finding lstdc++ & co:
    <path to> necessitas/android-ndk/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/../lib/gcc/arm-linux-androideabi/4.4.3/../../../../arm-linux-androideabi/bin/ld:
      error: cannot find -lstdc++
    
      Thanks to Aaron Richiger for reporting this ! :)

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export NECESSITAS_DIR="<path to the Necessitas SDK folder>"
+export NECESSITAS_DIR=/home/melf-tan/necessitas/
 # The Necessitas SDK contains not only Qt libraries built for Android, but also an
 # Android NDK, that can be used to build Shiboken & PySide for Android
 export ANDROID_NDK="${NECESSITAS_DIR}/android-ndk"
@@ -29,6 +29,7 @@ STL_LIBS="-L${STL_PATH}/4.4.3/libs/armeabi"
 CTYPE_INCLUDES="-I${ANDROID_NDK}/platforms/android-${ANDROID_PLATFORM_NUMBER}/arch-arm/usr/include"
 CTYPE_INCLUDES_RAW="${ANDROID_NDK}/platforms/android-${ANDROID_PLATFORM_NUMBER}/arch-arm/usr/include"
 ANDROID_INCLUDES="${ANDROID_NDK}/platforms/android-${ANDROID_PLATFORM_NUMBER}/arch-arm/usr/include:${STL_RAW_INCLUDES}"
+ANDROID_SYSROOT="${ANDROID_NDK}/platforms/android-${ANDROID_PLATFORM_NUMBER}/arch-arm"
 
 export MAKEFLAGS="-I${ANDROID_INCLUDES}"
 export INCLUDE=${ANDROID_INCLUDES}
@@ -41,13 +42,13 @@ export ANDROID_NDK=/home/melf-san/data/coding/SDK/android/necessitas/android-ndk
 export PATH="${ANDROID_BIN}:${ANDROID_NDK}:${ANDROID_NDK}/tools:$PATH"
 export PATH="${QT_DIR}/bin:$PATH"
 export ARCH="armeabi"
-export CFLAGS="-DANDROID -mandroid -fomit-frame-pointer -pipe "
+export CFLAGS="-DANDROID -mandroid -fomit-frame-pointer -pipe --sysroot ${ANDROID_SYSROOT}"
 SHIBOKEN_INCLUDE="-I${BUILD_DIR}/stage/include/shiboken"
 export CXXFLAGS="${CTYPE_INCLUDES} ${CFLAGS} ${STL_LIBS} ${STL_INCLUDES} ${CTYPE_INCLUDES} ${SHIBOKEN_INCLUDE} ${PYTHON_INCLUDES} ${STAGE_LIBS} -fPIC -fvisibility=hidden -Wno-strict-aliasing -rdynamic"
 
 #export LDFLAGS="--sysroot $ANDROID_NDK/platforms/android-14/arch-arm ${PYTHON_LIBZ} -Wl,-O1 -nostdlib -shared -lpython2.7 -lstdc++ -lc -lm"
 #export LDFLAGS="--sysroot $ANDROID_NDK/platforms/android-14/arch-arm ${PYTHON_LIBZ} -nostdlib -shared -lpython2.7 -lstdc++ -lc -lm"
-export LDFLAGS="--sysroot ${ANDROID_NDK}/platforms/android-${ANDROID_PLATFORM_NUMBER}/arch-arm ${PYTHON_LIBS} -nostdlib -shared -Wno-strict-aliasing -Wl,-O1 -Wl,-z,noexecstack -Wl,-shared,-Bsymbolic -Wl,--no-whole-archive -L${QT_DIR}/lib -lQtCore -lpython2.7 -lstdc++ -lsupc++ -lgcc -llog -lz -lm -ldl -lc"
+export LDFLAGS="--sysroot ${ANDROID_SYSROOT} ${PYTHON_LIBS} -nostdlib -shared -Wno-strict-aliasing -Wl,-O1 -Wl,-z,noexecstack -Wl,-shared,-Bsymbolic -Wl,--no-whole-archive -L${QT_DIR}/lib -lQtCore -lpython2.7 -lstdc++ -lsupc++ -lgcc -llog -lz -lm -ldl -lc"
 export CC="${ANDROID_BIN}/arm-linux-androideabi-gcc"
 export CXX="${ANDROID_BIN}/arm-linux-androideabi-g++"
 export AR="${ANDROID_BIN}/arm-linux-androideabi-ar"
